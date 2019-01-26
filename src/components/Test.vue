75fa007bf02f905c8f43df06d10d78c9a771620e
erkea <erkea@vilya.io>
2019-01-26 15:50:49 +0800

    搭建框架：vue和elementui

---
+++ src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <el-radio v-model="radio" label="1">备选项</el-radio>
+    <el-radio v-model="radio" label="2">备选项</el-radio>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      radio: '1'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
